f3e90df1a3b9b3fa5df27322385623c145865aeb
Tyler Cloetingh <tcloetingh@gmail.com>
2020-11-09 09:57:45 -0500

    angular / firebase / material setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule,
+    BrowserAnimationsModule, // storage
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyDYvnvWlJJ_z07RcYsIugf-LaitzspsrVw',
+    authDomain: 'home-calculators.firebaseapp.com',
+    databaseURL: 'https://home-calculators.firebaseio.com',
+    projectId: 'home-calculators',
+    storageBucket: 'home-calculators.appspot.com',
+    messagingSenderId: '602319978834',
+    appId: '1:602319978834:web:1936f095fc7f15c4507a8b',
+    measurementId: 'G-SHXGWKLEJV'
+  }
 };
 
 /*
