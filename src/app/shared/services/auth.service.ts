de1a93a16c73b67b690c253c3971b79a3bfd70de
Victor Bravo <airth@live.com>
2020-12-12 13:10:58 -0500

    my take on things

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -2,15 +2,12 @@ import { Injectable, NgZone } from '@angular/core';
 import { User } from '../services/user';
 // import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument
-} from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { auth } from 'firebase';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   userData: any; // Save logged in user data
@@ -20,25 +17,12 @@ export class AuthService {
     public afAuth: AngularFireAuth, // Inject Firebase auth service
     public router: Router,
     public ngZone: NgZone // NgZone service to remove outside scope warning
-  ) {
-    /* Saving user data in localstorage when
-    logged in and setting up null when logged out */
-    this.afAuth.authState.subscribe(user => {
-      if (user) {
-        this.userData = user;
-        localStorage.setItem('user', JSON.stringify(this.userData));
-        JSON.parse(localStorage.getItem('user'));
-      } else {
-        localStorage.setItem('user', null);
-        JSON.parse(localStorage.getItem('user'));
-      }
-    });
-  }
+  ) {}
 
   // Returns true when user is looged in and email is verified
   get isLoggedIn(): boolean {
     const user = JSON.parse(localStorage.getItem('user'));
-    return user !== null && user.emailVerified !== false ? true : false;
+    return user && user.emailVerified;
   }
 
   // Sign in with Google
@@ -50,13 +34,11 @@ export class AuthService {
   AuthLogin(provider) {
     return this.afAuth.auth
       .signInWithPopup(provider)
-      .then(result => {
-        this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
-        });
+      .then((result) => {
         this.SetUserData(result.user);
+        this.router.navigate(['/dashboard']);
       })
-      .catch(error => {
+      .catch((error) => {
         window.alert(error);
       });
   }
@@ -65,19 +47,8 @@ export class AuthService {
   sign up with username/password and sign in with social auth
   provider in Firestore database using AngularFirestore + AngularFirestoreDocument service */
   SetUserData(user) {
-    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
-      `users/${user.uid}`
-    );
-    const userData: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified
-    };
-    return userRef.set(userData, {
-      merge: true
-    });
+    this.userData = user;
+    localStorage.setItem('user', JSON.stringify(user));
   }
 
   // Sign out
